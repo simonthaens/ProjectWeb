5853bdacf8470d4c9323f42285f32db2d0f15aa5
Jorendss <jorentje_ds@hotmail.com>
2015-05-06 16:14:59 +0200

    tweede commit #origineel

--- application/views/template/view_footer.php
+++ application/views/template/view_footer.php
@@ -8,5 +8,5 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/project/assets/js/bootstrap.min.js"></script>
+    <script src="/ProjectWeb/assets/js/bootstrap.min.js"></script>
   </body>
