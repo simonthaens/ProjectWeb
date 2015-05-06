c866a5be455d945aaeaf645251d0f8676111a6d9
Jorendss <jorentje_ds@hotmail.com>
2015-05-06 16:06:37 +0200

    eerste commit

---
+++ application/views/template/view_footer.php
@@ -0,0 +1,12 @@
+	<div class="container">
+ 		<p>
+ 			&copy; 2015 - Awesome Site JEWEETZELLUFFFF
+ 		</p>
+	</div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/project/assets/js/bootstrap.min.js"></script>
+  </body>
