2a2ed31b4a4b54b68ccaed5b7c8fc82b4ed21180
Nick <nick_philippe@hotmail.com>
2015-05-17 17:16:30 +0200

    Revert "rdgdgfh"
    This reverts commit a124e9840279a2bba217d781e1c4aca87dea5c0b.

--- application/views/home.php
+++ application/views/home.php
@@ -337,19 +337,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <h2><span style="color:#FF4747;">Keep tuned:</span></h2>
+                    <h2>Connect to Start Bootstrap:</h2>
                 </div>
                 <div class="col-lg-6">
                     <ul class="list-inline banner-social-buttons">
-                         <li>
-                             <a href="https://www.facebook.com/TEDxPXL" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
-                         </li>
-                         <li>
-                             <a href="https://twitter.com/tedx" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                         </li>                         
-                         <li>
-                             <a href="https://www.linkedin.com/company/tedxuhasselt" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-                         </li>
+                        <li>
+                            <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                        </li>
                     </ul>
                 </div>
             </div>
