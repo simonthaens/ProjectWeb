33b81bde2df48dcf7b9a8c7adeab02f79b0d206d
Nick <nick_philippe@hotmail.com>
2015-05-17 21:21:34 +0200

    ttt

--- application/views/home.php
+++ application/views/home.php
@@ -218,10 +218,10 @@
     <div class="banner">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 col-lg-offset-11">
                     <h2><span style="color:#FF4747;">Keep tuned:</span></h2>
                 </div>
-                <div class="col-lg-6">
+                <div id="netwerkdiv" class="col-lg-6 col-lg-offset-9">
                     <ul class="list-inline banner-social-buttons">
                          <li>
                              <a href="https://www.facebook.com/TEDxPXL" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
