1305e5e01eafd0ffa8db01e8aaaaa5dd4f775196
Nick <nick_philippe@hotmail.com>
2015-05-18 13:38:07 +0200

    update2

--- application/controllers/default_Controller.php
+++ application/controllers/default_Controller.php
@@ -3,11 +3,16 @@
 class default_Controller extends CI_Controller{	
 	public function home($page = 'home')
 	{
+		$this->load->helper('url');
+		
 		if(!file_exists('application/views/'.$page.'.php')){
 			show_404();
 		}
 		
+		$this->load->view('template/nav');
 		$this->load->view($page);
+		$this->load->view('template/banner');
+		$this->load->view('template/footer');
 		
 		
 		//$data['title'] = 'HET WERKT';
@@ -16,6 +21,20 @@ class default_Controller extends CI_Controller{
 		$this->load->view('view_home');
 		$this->load->view('template/view_contact');		
 		$this->load->view('template/view_footer');*/
+	}	
+	
+	public function about($page = 'about')
+	{
+		$this->load->helper('url');
+		
+		if(!file_exists('application/views/'.$page.'.php')){
+			show_404();
+		}
+		
+		$this->load->view('template/nav');
+		$this->load->view($page);
+		$this->load->view('template/banner');
+		$this->load->view('template/footer');
 	}	
 }
 
