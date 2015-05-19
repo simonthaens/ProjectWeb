c4c1efc2dcccc0d139ae40d80d7a0dcc5b84682a
Nick <nick_philippe@hotmail.com>
2015-05-19 11:55:36 +0200

    dfvsd

--- application/views/about.php
+++ application/views/about.php
@@ -42,8 +42,8 @@
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">TEDxUHASSELT 2012</h2>
-                    <p class="lead">A special thanks to <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a> for providing the photographs that you see in this template. Visit their website to become a member.</p>
+                    <h2 class="section-heading">About TEDxUHasselt</h2>
+                    <p class="lead">TEDxUHasselt is an independently organized TED conference where speakers from around the world share cutting-edge ideas. During the breaks, attendees connect with each other through various interactive activities.<br><br>TEDxUHasselt was founded in 2011 by three Hasselt University students and has remained a student-driven organization ever since. !!!!!Meet our team members!</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="assets/pictures/ipad.png" alt="">
@@ -60,11 +60,18 @@
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">TEDxUHASSELTLIVE 2015</h2>
-                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+                    <h2 class="section-heading">About TEDx</h2>
+                    <p class="lead">About TEDx, x = independently organized event In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TEDTalks video and live speakers combine to spark deep discussion and connection in a small group. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)</p>
                 </div>
-                <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="assets/pictures/dog.png" alt="">
+                <div class="col-lg-5 col-sm-pull-6  col-sm-6" style="padding-top: 5em;">
+                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" height="326" width="444">
+                    	<param name="bgcolor" value="#ffffff">
+                    	<param name="flashvars" value="vu=http://video.ted.com/talks/embed/TEDShortRemix_2008-embed_high.flv&amp;su=http://images.ted.com/images/ted/tedindex/embed-posters/TEDShortRemix.2008-embed_thumbnail.jpg&amp;vw=432&amp;vh=240&amp;ap=0">
+                    	<param name="src" value="http://video.ted.com/assets/player/swf/EmbedPlayer.swf">
+                    	<param name="wmode" value="transparent">
+                    	<param name="allowfullscreen" value="true">
+                    	<embed allowfullscreen="true" bgcolor="#ffffff" flashvars="vu=http://video.ted.com/talks/embed/TEDShortRemix_2008-embed_high.flv&amp;su=http://images.ted.com/images/ted/tedindex/embed-posters/TEDShortRemix.2008-embed_thumbnail.jpg&amp;vw=432&amp;vh=240&amp;ap=0" height="326" src="http://video.ted.com/assets/player/swf/EmbedPlayer.swf" title="Adobe Flash Player" type="application/x-shockwave-flash" width="444" wmode="transparent">                    		
+                    	</object>
                 </div>
             </div>
         </div>
@@ -78,23 +85,11 @@
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">TEDxPXL 2014</h2>
-                    <div class="row">
-						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELT 2016</a>
-						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELTSALON 2013</a>
-					</div>
-					<div class="row">
-  						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELTLIVE 2015</a>
-  						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELT 2012</a>
-					</div>
-					<div class="row">
-  						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELT 2014</a>
-  						<a href="" class="btn btn-danger" id="eventslink">TEDxUHASSELT 2011</a>
-					</div>
+                    <h2 class="section-heading">About TED</h2>
+                    <p class="lead">TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 25 years ago, TED has grown to support those world-changing ideas with multiple initiatives. The annual TED Conference invites the world's leading thinkers and doers to speak for 18 minutes. Their talks are then made available, free, at TED.com. TED speakers have included Bill Gates, Al Gore, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson, Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Isabel Allende and UK Prime Minister Gordon Brown. The annual TED Conference takes place in Long Beach, California, with simulcast in Palm Springs; TEDGlobal is held each year in Oxford, UK.</p>					
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" id="eventsimg" src="assets/pictures/events1.jpg" alt="">
-                    <img class="img-responsive" id="eventsimg" src="assets/pictures/events2.png" alt="">
+                    <p class="lead">TED's media initiatives include TED.com, where new TEDTalks are posted daily, and the Open Translation Project, which provides subtitles and interactive transcripts as well as the ability for any TEDTalk to be translated by volunteers worldwide. TED has established the annual TED Prize, where exceptional individuals with a wish to change the world are given the opportunity to put their wishes into action; TEDx, which offers individuals or groups a way to organize independent, TED-like events around the world; and the TEDFellows program, helping world-changing innovators from around the globe to become part of the TED community and, with its help, amplify the impact of their remarkable projects and activities. Follow TED on Twitter at twitter.com/TEDTalks, or on Facebook at facebook.com/TED.</p>
                 </div>
             </div>
         </div>
@@ -108,14 +103,11 @@
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>              
-                    <h2 class="section-heading">TEDxUHASSELTSALON 2013</h2>
-                   
+                    <h2 class="section-heading">Team</h2>
+                    
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                	<hr class="section-heading-spacer">
-                    <div class="clearfix"></div> 
-                
-              
+            
                 </div>
             </div>
         </div>
