900a33df9597845269311b51b76d40012ac8cba9
Nick <nick_philippe@hotmail.com>
2015-05-19 16:03:43 +0200

    gerg

--- application/views/contact.php
+++ application/views/contact.php
@@ -32,7 +32,7 @@
                        <span class='help-block'> <?php echo form_error('message') ?> </span>
                     </div>
                             
-                    <button type="submit" id="submit" value="submit" name="submitted" class="btn btn-default btn-lg">Send</button>
+                    <button type="submit" id="submit" value="submit" name="submitted" class="btn btn-danger" style="width:30%; height:5%;">Send</button>
 
                     <?php
                         echo form_close();
