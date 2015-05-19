af6527ee862767f0f881346af01e5f8f4dbeee65
Nick <nick_philippe@hotmail.com>
2015-05-19 17:08:02 +0200

    wefw

--- application/views/template/banner.php
+++ application/views/template/banner.php
@@ -1,10 +1,22 @@
 <div class="banner">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-lg-offset-11">
-                    <h2><span style="color:#d9534f;">Keep tuned:</span></h2>
-                </div>
-                <div id="netwerkdiv" class="col-lg-6 col-lg-offset-9">
+            	<div class="col-lg-3">
+                    <h2><span style="color:#d9534f;">Twitter:</span></h2>
+                    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+					<div class="fb-page" data-href="https://www.facebook.com/TEDxUHasselt" data-width="300" data-height="300" data-hide-cover="false" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/TEDxUHasselt"><a href="https://www.facebook.com/TEDxUHasselt">TEDxUHasselt</a></blockquote></div></div>
+             
+                </div>                
+                <div id="netwerkdiv" class="col-lg-6">
+                	<h2 style="text-align: center; padding-bottom: 0.5em;"><span style="color:#d9534f;">Keep tuned:</span></h2>
                     <ul class="list-inline banner-social-buttons">
                          <li>
                              <a href="https://www.facebook.com/TEDxPXL" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
@@ -16,6 +28,10 @@
                              <a href="https://www.linkedin.com/company/tedxuhasselt" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
                          </li>
                     </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h2><span style="color:#d9534f;">Facebook:</span></h2>
+             
                 </div>
             </div>
         </div>
--- application/views/view_home.php
+++
@@ -1,4 +0,0 @@
-<div class="container">
-	<h1>Bootstrap 3 + CodeIgniter = HET WERKT KUTTEEUUUHHHH!!!!!!!!</h1>
-	<p class="lead">zoals ik al zei, het werkt hihi</p>
-</div>
