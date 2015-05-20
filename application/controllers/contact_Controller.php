f97358f84c885fd0519d6936979ac2cb2c57b693
Joshua Gielen <joshua.gielen@student.pxl.be>
2015-05-20 12:45:08 +0200

    dqfsdf

--- application/controllers/contact_Controller.php
+++ application/controllers/contact_Controller.php
@@ -2,7 +2,7 @@
 
 class Contact_controller extends CI_Controller {
                
-	public function __construct()
+public function __construct()
     {
         parent::__construct();
         $this->load->helper(array('form','url'));
@@ -11,6 +11,31 @@ class Contact_controller extends CI_Controller {
 
     function index()
     {
+        $config = Array(
+                'protocol' => 'smtp',
+                'smtp_host' => 'ssl://smtp.googlemail.com',
+                'smtp_port' => 465,
+                'smpt_user' => 'joshua.gielen@gmail.com',
+                'smpt_pass' => 'AD7abyrf'
+        );
+
+
+
+        $this->load->library('email',$config); 
+        $this->email->set_newline("\r\n");
+
+        $this->email->from('joshua.gielen@gmail.com', 'Joshua Gielen');
+        $this->email->to('joshua.gielen@gmail.com');
+        $this->email->subject('test');
+        $this->email->message('this is a test, hope its working');
+
+        if($this->email->send()){
+            echo "email successfully send";
+        }else{
+            show_error($this->email->print_debugger());
+        }
+/*
+
         //set validation rules
         $this->form_validation->set_rules('name', 'Name', 'trim|required|callback_alpha_space_only');
         $this->form_validation->set_rules('email', 'Emaid ID', 'trim|required|valid_email');
@@ -21,7 +46,8 @@ class Contact_controller extends CI_Controller {
         if ($this->form_validation->run() == FALSE)
         {
             //validation fails
-           //google maps
+        
+            //google maps
             $this->load->library('googlemaps');
             $config['center'] = 'UHasselt Diepenbeek';
             $config['zoom'] = '15';
@@ -32,11 +58,11 @@ class Contact_controller extends CI_Controller {
             $data['map'] = $this->googlemaps->create_map();
 
             $this->load->view('template/nav');
-            $this->load->view($page = 'home');
+            $this->load->view($page='home');
             $this->load->view('contact',$data);
             $this->load->view('template/banner');
             $this->load->view('template/footer');
-            
+
 
             
         }
@@ -45,30 +71,11 @@ class Contact_controller extends CI_Controller {
             //get the form data
             $name = $this->input->post('name');
             $from_email = $this->input->post('email');
-            $subject = "Contact form TEDx";
+            $subject = "Message from contact form TEDx";
             $message = $this->input->post('message');
 
             //set to_email id to which you want to receive mails
             $to_email = 'joshua.gielen@gmail.com';
-
-
-            $this->load->library('email');
-
-            $this->email->from($from_email, $name);
-            $this->email->to($to_email);
-            $this->email->subject($subject);
-            $this->email->message($message); 
-
-
-
-
-
-
-
-
-
-
-            /*
 
             //configure email settings
             $config['protocol'] = 'smtp';
@@ -87,24 +94,20 @@ class Contact_controller extends CI_Controller {
             $this->email->from($from_email, $name);
             $this->email->to($to_email);
             $this->email->subject($subject);
-            $this->email->message($message); */
-
-            print "sent email before";
+            $this->email->message($message);
             if ($this->email->send())
             {
-                print "sent email true";
                 // mail sent
                 $this->session->set_flashdata('msg','<div class="alert alert-success text-center">Your mail has been sent successfully!</div>');
                 redirect('contactform/index');
             }
             else
             {
-                print "sent email false";
                 //error
                 $this->session->set_flashdata('msg','<div class="alert alert-danger text-center">There is error in sending mail! Please try again later</div>');
                 redirect('contactform/index');
             }
-        }
+        }*/
     }
     
     //custom validation function to accept only alphabets and space input
--- application/views/contact.php
+++ application/views/contact.php
@@ -7,7 +7,7 @@
                     <h2 class="secion-heading">Contact us</h2>
 
                     <?php
-                        $attributes = array('class' => '', 'id' => '');
+                        $attributes = array("class" => "form-horizontal", "name" => "contactform");
                         $this->load->helper('form');
                         echo form_open(base_url() . "contact_Controller/index", $attributes); 
                     ?>
@@ -15,33 +15,31 @@
                      <div class="form-group" >
                         <label for="name">Name</label>
                         <!--<input type="text" class="form-control" id="inputName" placeholder="Enter name" name="name" value='<?php echo htmlentities($inputName)?>'>-->
-                        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" value=''>
-                        <span class='error'> <?php echo form_error('name') ?> </span>
+                        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" value='<?php echo set_value('name'); ?>'>
+                        <span class='text-danger'> <?php echo form_error('name') ?> </span>
                                  
                     </div>
 
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" >
-                        <span class='error'> <?php echo form_error('email') ?> </span>
+                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"value='<?php echo set_value('email'); ?>' >
+                        <span class='text-danger'> <?php echo form_error('email') ?> </span>
                     </div>
 
                     <div class="form-group">
                         <label for="inputMessage">Your message</label>
-                        <textarea class="form-control" id="inputMessage" placeholder="Enter message" name="message" ></textarea>
-                       <span class='help-block'> <?php echo form_error('message') ?> </span>
+                        <textarea class="form-control" id="inputMessage" placeholder="Enter message" name="message"  value='<?php echo set_value('message'); ?>'></textarea>
+                       <span class='text-danger'> <?php echo form_error('message') ?> </span>
                     </div>
                         
                     <button type="submit" id="submit" value="submit" name="submitted" class="btn btn-danger" style="width:30%; height:5%;">Send</button>
-
-                    <?php
-                        echo form_close();
-
-                    ?>
-
-
+                    
+                    <?php echo form_close(); ?>
+                    
 
                 </div>
+                    
+
 
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                     <?php echo $map['js']; ?>
